2bd33231d98336cb28fe9d00ac96f06d18ac26aa
Guillaume Serrière <guillaume.serriere@telecomnancy.net>
2017-05-18 20:43:50 +0200

    Add action status (next step) and step type.
    Signed-off-by: Guillaume Serrière <guillaume.serriere@telecomnancy.net>

--- actionHandler/src/main/java/fr/esialrobotik/ActionDescriptor.java
+++ actionHandler/src/main/java/fr/esialrobotik/ActionDescriptor.java
@@ -1,12 +1,10 @@
 package fr.esialrobotik;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -21,9 +19,11 @@ public class ActionDescriptor {
   private int time;
 
   private List<Step> stepList;
+  private int stepIndex;
 
   public ActionDescriptor(JsonObject object) {
     stepList = new ArrayList<Step>();
+    stepIndex = -1;
 
     objectiveId = object.get("id").getAsInt();
     points = object.get("points").getAsInt();
@@ -44,12 +44,37 @@ public class ActionDescriptor {
     return res;
   }
 
+  public boolean isActionStarted() {
+    return stepIndex < 0;
+  }
+
+  public boolean hasNextStep() {
+    return this.stepIndex < stepList.size() - 1 ;
+  }
+
+  public Step getNextStep() {
+    ++stepIndex;
+    return this.stepList.get(stepIndex);
+  }
+
+  public Step getCurrentStep() {
+    return stepList.get(stepIndex);
+  }
+
   public static void main(String[] args) throws FileNotFoundException {
     JsonParser parser = new JsonParser();
     JsonElement elt = parser.parse(new JsonReader(new FileReader("actionHandler/configAction.json")));
 
     ActionDescriptor actionDescriptor = new ActionDescriptor(elt.getAsJsonObject());
     System.out.println(actionDescriptor);
-  }
 
+    System.out.println(actionDescriptor.getNextStep().toString());
+    System.out.println(actionDescriptor.hasNextStep());
+    System.out.println(actionDescriptor.getNextStep().toString());
+    System.out.println(actionDescriptor.hasNextStep());
+    System.out.println(actionDescriptor.getNextStep().toString());
+    System.out.println(actionDescriptor.hasNextStep());
+    System.out.println(actionDescriptor.getNextStep().toString());
+    System.out.println(actionDescriptor.hasNextStep());
+  }
 }
--- actionHandler/src/main/java/fr/esialrobotik/Step.java
+++ actionHandler/src/main/java/fr/esialrobotik/Step.java
@@ -10,7 +10,9 @@ import fr.esialrobotik.data.table.Point;
 public class Step {
   public enum Type{
     DEPLACEMENT,
-    MANIPULATION
+    MANIPULATION,
+    DEPLACEMENT_ALWAYS, //Not the best name. If someone as better enjoy
+    ROTATION
   }
 
   private int stepId;
@@ -19,6 +21,7 @@ public class Step {
   private int idAction;
   // Start position of the action
   private Position position;
+  private Type actionType;
 
   public Step(JsonObject configNode) {
     this.stepId = configNode.get("id").getAsInt();
@@ -26,12 +29,26 @@ public class Step {
     this.position = new Position(configNode.get("positionX").getAsInt(),
             configNode.get("positionY").getAsInt(),
             configNode.get("theta").getAsDouble());
+    String type = configNode.get("type").getAsString();
+    if(type.equals("deplacement")) {
+      actionType = Type.DEPLACEMENT;
+    }
+    else if(type.equals("deplacement_always")) {
+      actionType = Type.DEPLACEMENT_ALWAYS;
+    }
+    else if(type.equals("rotation")) {
+      actionType = Type.ROTATION;
+    }
+    else if(type.equals("manipulation")) {
+      actionType = Type.MANIPULATION;
+    }
   }
 
   public String toString() {
     String res  = "id : " + stepId;
     res += "\nposition : " + position;
     res += "\nactionId: " + idAction;
+    res += "\ntype: " + actionType;
     return res;
   }
 }
