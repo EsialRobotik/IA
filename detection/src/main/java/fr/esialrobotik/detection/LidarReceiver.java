b0b19f5388d6239160b82475c4e1a67d6eea06fd
Serrière Guillaume <guillaume.serriere@telecomnancy.net>
2017-05-22 20:00:50 +0200

    Add some logging to now if lidar is working.
    Signed-off-by: Serrière Guillaume <guillaume.serriere@telecomnancy.net>

--- detection/src/main/java/fr/esialrobotik/detection/LidarReceiver.java
+++ detection/src/main/java/fr/esialrobotik/detection/LidarReceiver.java
@@ -1,6 +1,8 @@
 package fr.esialrobotik.detection;
 
+import esialrobotik.ia.utils.log.LoggerFactory;
 import esialrobotik.ia.utils.rplidar.*;
+import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
 
@@ -10,14 +12,21 @@ import javax.inject.Inject;
  * Created by icule on 22/05/17.
  */
 public class LidarReceiver implements RpLidarListener {
+    private Logger logger;
     @Inject
     public LidarReceiver() {
+        this.logger = LoggerFactory.getLogger(LidarReceiver.class);
     }
 
 
     @Override
     public void handleMeasurement(RpLidarMeasurement rpLidarMeasurement) {
-
+        double deg = rpLidarMeasurement.angle / 64.0;
+        double r = rpLidarMeasurement.distance / 4.0;
+        if (rpLidarMeasurement.start)
+            System.out.println();
+        if( r < 1000 && r > 1)
+            logger.debug(rpLidarMeasurement.start + " %3d   theta = %6.2f r = %10.2f\n", rpLidarMeasurement.quality, deg, r);
     }
 
     @Override
